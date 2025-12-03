72e81719c969fe102d7806659fda9eae759a7dcc
shahidmalik4 <shahidawan145@gmail.com>
2025-12-03 22:05:46 +0500

    dbt and airflow connection

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,73 +1,37 @@
-from airflow.models.dag import DAG
-from airflow.utils.task_group import TaskGroup
-from airflow.models.baseoperator import chain
-from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig, ExecutionConfig
+from cosmos import DbtDag, ProjectConfig, ProfileConfig
 from cosmos.profiles.postgres import PostgresUserPasswordProfileMapping
 from datetime import datetime
-import os
 from pathlib import Path
 
-# Paths
+
 DBT_PROJECT_PATH = Path("/opt/airflow/dbt/dbt_project")
-DBT_EXECUTABLE_PATH = f"{os.getenv('AIRFLOW_HOME')}/dbt_venv/bin/dbt"
 POSTGRES_CONN_ID = "postgres_local"
 
-# Cosmos configs
 _project_config = ProjectConfig(dbt_project_path=DBT_PROJECT_PATH)
-_execution_config = ExecutionConfig(dbt_executable_path=DBT_EXECUTABLE_PATH)
 profile_config = ProfileConfig(
     profile_name="dbt_project",
     target_name="dev",
     profile_mapping=PostgresUserPasswordProfileMapping(
         conn_id=POSTGRES_CONN_ID,
-        profile_args={"schema": "staging", "dbname": "warehouse"},
+        profile_args={"dbname": "warehouse", "schema": "staging"},
     ),
 )
 
-# Define DAG
-with DAG(
+dbt_project_dag = DbtDag(
     dag_id="data_pipeline_dbt_etl",
     schedule="@daily",
     start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=["dbt", "cosmos"],
-) as dag:
 
-    # --- Staging TaskGroup ---
-    staging_group = DbtTaskGroup(
-        group_id="staging",
-        project_config=_project_config,
-        profile_config=profile_config,
-        execution_config=_execution_config,
-        default_args={"models": "staging.*"},
-    )
-
-    # --- Dim TaskGroup ---
-    dims_group = DbtTaskGroup(
-        group_id="dims",
-        project_config=_project_config,
-        profile_config=profile_config,
-        execution_config=_execution_config,
-        default_args={"select": "marts.dims.*"},
-    )
-
-    # --- Fact TaskGroup ---
-    facts_group = DbtTaskGroup(
-        group_id="facts",
-        project_config=_project_config,
-        profile_config=profile_config,
-        execution_config=_execution_config,
-        default_args={"select": "marts.facts.*"},
-    )
-
-    # --- Analytics TaskGroup ---
-    analytics_group = DbtTaskGroup(
-        group_id="analytics",
-        project_config=_project_config,
-        profile_config=profile_config,
-        execution_config=_execution_config,
-        default_args={"select": "analytics.*"},
-    )
-
-    # Set dependencies between TaskGroups
-    chain(staging_group, dims_group, facts_group, analytics_group)
+    # Cosmos Dbt Arguments
+    project_config=_project_config,
+    profile_config=profile_config,
+    
+    # Default arguments
+    default_args={
+        "owner": "airflow",
+        "retries": 1,
+    },
+    
+)
--- dbt/dbt_project/models/analytics/top_products.sql
+++ dbt/dbt_project/models/analytics/top_products.sql
@@ -1,12 +1,12 @@
 -- Top products by revenue and quantity sold.
 
 select
-    p.part_id,
-    p.name as part_name,
-    sum(l.extended_price * (1 - l.discount)) as revenue,
-    sum(l.quantity) as quantity_sold,
-    row_number() over (order by sum(l.extended_price * (1 - l.discount)) desc) as rank_by_revenue
-from {{ ref('stg_lineitems') }} l
-left join {{ ref('stg_part') }} p on l.part_id = p.part_id
-group by 1,2
+    dp.part_key,
+    dp.part_name,
+    sum(fl.net_price) as revenue,
+    sum(fl.quantity) as quantity_sold,
+    row_number() over (order by sum(fl.net_price) desc) as rank_by_revenue
+from {{ ref('fact_lineitem') }} fl
+left join {{ ref('dim_part') }} dp on fl.part_key = dp.part_key
+group by 1, 2
 order by revenue desc
